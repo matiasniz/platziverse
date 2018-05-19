55395cb09bcf2837de677661611f0c2c0af052f1
miscorrales <matiasniz@icloud.com>
2018-05-19 07:38:57 -0300

    avance

--- platziverse-db/index.js
+++ platziverse-db/index.js
@@ -3,8 +3,21 @@
 const setupDatabase = require('./lib/db')
 const setupAgentModel = require('./models/agent')
 const setupMetricModel = require('./models/metric')
-
+const defaults = require('defaults')
+const setupAgent = require('./lib/agent')
 module.exports = async function (config) {
+  config = defaults(config, {
+    dialect: 'sqlite',
+    pool: {
+      max: 10,
+      min: 0,
+      idle: 10000
+    },
+    query: {
+      raw: true // entrega objetos json sencillos basicos
+    }
+  })
+
   const sequelize = setupDatabase(config)
   const AgentModel = setupAgentModel(config)
   const MetricModel = setupMetricModel(config)
@@ -18,7 +31,7 @@ module.exports = async function (config) {
     await sequelize.sync({ force: true })
   }
 
-  const Agent = {}
+  const Agent = setupAgent(AgentModel)
   const Metric = {}
 
   return {
